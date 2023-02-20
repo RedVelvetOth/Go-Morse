31bd9ad874fb87012af0964a9da25cda08dd381e
RedVelvetOth <boumyaothmane@gmail.com>
2023-02-20 18:55:33 +0100

    Made a GUI, using fyne

--- morse.go
+++ morse.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
 	"os"
 	"toys/morse/morse"
-
-	"github.com/f1bonacc1/glippy"
 )
 
 func main() {
+	os.Setenv("FYNE_THEME", "dark")
 
-	userCli := flag.Bool("d", false, "Demorse The Morse Code")
-	flag.Parse()
-
-	if *userCli {
-		fmt.Println("What do you wanna Demorse: ")
-		reader := bufio.NewReader(os.Stdin)
-		giberish, _ := reader.ReadString('\n')
-		memes := morse.Demorser(giberish)
-		glippy.Set(memes)
-		fmt.Printf("%s", memes)
-	} else {
-		fmt.Println("What do you wanna morse: ")
-		reader := bufio.NewReader(os.Stdin)
-		giberish, _ := reader.ReadString('\n')
-		memes := morse.Morser(giberish)
-		glippy.Set(memes)
-		fmt.Printf("%s", memes)
-	}
-
+	morse.Gui()
 }
--- morse/morsing.go
+++ morse/morsing.go
@@ -2,6 +2,12 @@ package morse
 
 import (
 	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
 )
 
 var myMap = map[string]string{
@@ -78,4 +84,44 @@ func Demorser(s string) (v string) {
 		v = v + reverseMap[string(x)]
 	}
 	return
+}
+
+func Gui() {
+	myapp := app.New()
+	mywindow := myapp.NewWindow("Morser Or Demorser")
+
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Do What Ya Wanna do")
+
+	button_m := widget.NewButtonWithIcon("Morser", theme.ContentCopyIcon(), func() {
+		text := Morser(input.Text)
+		input.SetText(text)
+		mywindow.Clipboard().SetContent(text)
+	})
+
+	button_dm := widget.NewButtonWithIcon("Demorser", theme.ContentCopyIcon(), func() {
+		text := Demorser(input.Text)
+		input.SetText(text)
+		mywindow.Clipboard().SetContent(text)
+	})
+
+	mywindow.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewBorderLayout(
+				input,
+				nil,
+				nil,
+				nil,
+			),
+			input,
+			fyne.NewContainerWithLayout(
+				layout.NewGridLayout(2),
+				button_m,
+				button_dm,
+			),
+		),
+	)
+	mywindow.Resize(fyne.NewSize(400, 50))
+	mywindow.ShowAndRun()
+
 }
